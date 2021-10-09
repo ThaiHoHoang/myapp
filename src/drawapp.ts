fceba9f50a9e7f2c92a463b59af3fe483601d897
HoangThai <HoangThai@Macintoshs-MacBook-Air.local>
2021-10-09 23:31:04 +0700

    Update day5

--- src/drawapp.ts
+++ src/drawapp.ts
@@ -5,30 +5,22 @@ export class DrawingApp {
     constructor() {
         let canvas = document.getElementById('canvas') as HTMLCanvasElement;
         let context = canvas.getContext("2d");
-        context.lineCap = 'round';
-        context.lineJoin = 'round';
-        context.strokeStyle = 'red';
-        context.fillStyle = 'dark';
-        context.lineWidth = 1;
-    
         this.canvas = canvas;
         this.context = context;
     }
 
-    public drawBird(x1 : number, y1 : number) {
+    public drawBird(x1 : number, y1 : number, w : number, h : number) {
         let context = this.context;
         var img = new Image();
-        img.src = './img/bird.png';
-        img.onload;
-        context.drawImage(img,x1,y1);
+        img.src = '../src/img/bird.png';
+        context.drawImage(img,x1,y1,w,h);
         
     }
-    public drawPice(x1 : number, y1 : number) {
+    public drawBackGround(x1 : number, y1 : number, w : number, h : number) {
         let context = this.context;
         var img = new Image();
-        img.src = './img/ongduoi.png';
-        img.onload;
-        context.drawImage(img,x1,y1);
+        img.src = '../src/img/nenchinh.png';
+        context.drawImage(img,x1,y1,w,h);
         
     }
 
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,81 @@
 import {DrawingApp } from "./drawapp";
+import { DrawPipe} from "./pipe"
 
 var canvas : HTMLCanvasElement;
 var ctx : CanvasRenderingContext2D;
+var event: KeyboardEvent;
+let y: number;
+let x: number;
+let g: number;
+let drawing = new DrawingApp;
+let drawPipe: DrawPipe;
+let drawPipe2: DrawPipe;
+let isJump = false;
+let count = 0;
+
 function gameloop() {
     drawing.clearCanvas();
     ctx.fillStyle = '#8FBC8F';
-    ctx.fillRect(0,0,2000,5000); 
+    ctx.fillRect(0,0,2000,5000);
+    drawing.drawBackGround(0,0,canvas.width,canvas.height);
+    processInput();
+    update();
     render();
     requestAnimationFrame(gameloop)
 }
 window.onload = () => {
     canvas = document.getElementById('canvas') as HTMLCanvasElement;
     ctx = canvas.getContext('2d');
+    y = 0;
+    x = canvas.width;
+    g = 1;
+    drawPipe = new DrawPipe(x);
+    drawPipe2 = new DrawPipe(x+500);
+    document.addEventListener("keydown",onKeyDown);
     gameloop();
 }
-let drawing = new DrawingApp();
 
+function onKeyDown(this: HTMLCanvasElement, evt: KeyboardEvent) {
+    console.log("Received event!");
+    event = evt;
+}
 
-canvas = document.getElementById('canvas') as HTMLCanvasElement;
-ctx = canvas.getContext('2d');
-let y = 0;
-let x = 450;
-let g = 1;
+function processInput() {
+    if (event != null) {
+        switch(event.key){
+            case "u":
+                isJump = true;
+        }
+        event = null;
+    }
+}
 
 function render() {
-    drawing.drawPice(x,350);
-    drawing.drawBird(50,y);
-    if(y<=500 || x >= -10) {
-        x -= g*3;
-        y += g*5;
+   drawPipe.drawPipe();
+    drawPipe2.drawPipe();
+    drawing.drawBird(50,y, 60,50);
+}
+function update() {
+    drawPipe2.update();
+    drawPipe.update();
+    if(y<=canvas.height ) {
+        
+        if (isJump == true) {
+            if (count < 10) {
+                console.log("Bird jumped! count: " + count.toString());
+                y -= g*7;
+                count++;
+            }
+            else {
+                count = 0;
+                isJump = false;
+            }
+        }
+        else {
+            y += g*5;
+        }
     }
     else {
-        x = 450;
         y = 0;
-    }    
+    }
 }
